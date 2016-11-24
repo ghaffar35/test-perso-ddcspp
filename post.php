b9fb2f4e2bdebc7adeb7006e7ca669b67c19b3c0
Megaju <julen_malle@outlook.fr>
2016-11-24 23:29:04 +0100

    début post article pour demain

--- post.php
+++ post.php
@@ -4,8 +4,12 @@
 include('singleton.php');
 
 // add a user
-$req = $bdd->prepare('INSERT INTO user (pseudo, mail, pwd, id_role) VALUES(?, ?, ?, ?)');
-$req->execute(array($_POST['pseudo'], $_POST['mail'], $_POST['pwd'], $_POST['id_role']));
+$c_user = $bdd->prepare('INSERT INTO user (pseudo, mail, pwd, id_role) VALUES(?, ?, ?, ?)');
+$c_user->execute(array($_POST['pseudo'], $_POST['mail'], $_POST['pwd'], $_POST['id_role']));
+
+// add a post
+$c_post = $bdd->prepare('INSERT INTO post (id_user, title, description, date, url, type) VALUES(?, ?, ?, ?, ?, ?)');
+$c_post->execute(array($_POST['pseudo'], $_POST['mail'], $_POST['pwd'], $_POST['id_role']));
 
 // redirection
 header('Location: /bdd_ddcspp');
