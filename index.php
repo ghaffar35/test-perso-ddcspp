dd893d82ddb5c39871d6caeb1870aa624d9b6f12
Megaju <julen_malle@outlook.fr>
2016-11-24 19:55:39 +0100

    base html/css

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test BDD DDSCPP</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Test BDD DDCSPP</h1>
+            <h2>1er test de base de donnée pour le projet DDCSPP de la Code Académie de Rennes (FACE - SIMPLON)</h2>
+        </header>
+        
+        <section>
+            <!-- contenue du site ici - les test - tout ça, tout ça -->
+            <h3>Titre de section</h3>
+        </section>
+        
+        <footer>
+            <p>Julien Malle, 2016. Projet de la Code Académie de Rennes (FACE - SIMPLON).</p>
+        </footer>
+    </div>
+</body>
+</html>
